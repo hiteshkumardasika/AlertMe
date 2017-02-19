bf974d9d340d0e89c3976d0f0749f77a2d676f07
hdasika <hdasika@indiana.edu>
2017-02-19 13:39:04 -0500

    Contains Directions API. It draws all routes between two locations

--- app/src/main/java/com/example/root/alertme/MainActivity.java
+++ app/src/main/java/com/example/root/alertme/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.root.alertme;
 
-import android.*;
 import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -9,9 +11,11 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -23,32 +27,56 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.directions.route.AbstractRouting;
+import com.directions.route.Route;
+import com.directions.route.RouteException;
+import com.directions.route.Routing;
+import com.directions.route.RoutingListener;
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceDetectionApi;
+import com.google.android.gms.location.places.PlaceLikelihoodBuffer;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.internal.PlaceEntity;
 import com.google.android.gms.location.places.ui.PlaceAutocomplete;
-import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
-import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback, LocationListener {
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback, LocationListener,
+        GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, RoutingListener, ResultCallback<Status> {
 
     FloatingActionButton fab1, fab2, fab3;
     boolean isFABOpen = false;
-    Place place;
+    Place destination;
+    LatLng sourceLatLng;
     GoogleMap map;
+    ProgressDialog progressDialog;
+    NotificationManager notificationManager;
+    private List<Polyline> polylines;
+    private static final int[] COLORS = new int[]{R.color.primary_dark, R.color.primary, R.color.primary_light, R.color.accent, R.color.primary_dark_material_light};
 
 
     @Override
@@ -107,15 +135,16 @@ public class MainActivity extends AppCompatActivity
                 }
             }
         });
+        notificationManager= (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 1) {
             if (resultCode == RESULT_OK) {
-                place = PlaceAutocomplete.getPlace(this, data);
-                map.addMarker(new MarkerOptions().position(place.getLatLng()).title("Destination"));
-                map.animateCamera(CameraUpdateFactory.newLatLng(place.getLatLng()));
+                destination = PlaceAutocomplete.getPlace(this, data);
+                map.addMarker(new MarkerOptions().position(destination.getLatLng()).title("Destination"));
+                map.animateCamera(CameraUpdateFactory.newLatLng(destination.getLatLng()));
             } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
                 Status status = PlaceAutocomplete.getStatus(this, data);
                 Toast.makeText(MainActivity.this, status.getStatusMessage(), Toast.LENGTH_SHORT).show();
@@ -124,6 +153,21 @@ public class MainActivity extends AppCompatActivity
                 Toast.makeText(MainActivity.this, "Cancelled ", Toast.LENGTH_SHORT).show();
             }
         }
+        drawingPath();
+    }
+
+    protected void drawingPath() {
+        progressDialog = ProgressDialog.show(this, "Please wait.",
+                "Fetching route information.", true);
+
+        Routing routing = new Routing.Builder()
+                .withListener(this)
+                .travelMode(AbstractRouting.TravelMode.DRIVING)
+                .alternativeRoutes(true)
+                .waypoints(sourceLatLng, destination.getLatLng())
+                .build();
+        routing.execute();
+
     }
 
 
@@ -199,7 +243,6 @@ public class MainActivity extends AppCompatActivity
 
         // Create a criteria object to retrieve provider
         Criteria criteria = new Criteria();
-
         // Get the name of the best provider
         String provider = locationManager.getBestProvider(criteria, true);
 
@@ -214,10 +257,18 @@ public class MainActivity extends AppCompatActivity
         double longitude = myLocation.getLongitude();
 
         // Create a LatLng object for the current location
-        LatLng latLng = new LatLng(latitude, longitude);
+        sourceLatLng = new LatLng(latitude, longitude);
 
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+        map.animateCamera(CameraUpdateFactory.newLatLngZoom(sourceLatLng, 15));
+/*
+        PendingResult<PlaceLikelihoodBuffer> currentPlace = Places.PlaceDetectionApi.getCurrentPlace(this, null);
+        currentPlace.setResultCallback(new ResultCallback<PlaceLikelihoodBuffer>() {
+            @Override
+            public void onResult(@NonNull PlaceLikelihoodBuffer placeLikelihoods) {
 
+            }
+        });
+*/
     }
 
     @Override
@@ -240,5 +291,112 @@ public class MainActivity extends AppCompatActivity
         fab2.animate().translationY(0);
         fab3.animate().translationY(0);
 
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onRoutingFailure(RouteException e) {
+        // The Routing request failed
+        progressDialog.dismiss();
+        if (e != null) {
+            Toast.makeText(this, "Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "Something went wrong, Try again", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onRoutingStart() {
+        Toast.makeText(this, "Starting The routing Request!!!", Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    @Override
+    public void onRoutingSuccess(ArrayList<Route> route, int shortestRouteIndex) {
+
+        progressDialog.dismiss();
+        CameraUpdate center = CameraUpdateFactory.newLatLng(sourceLatLng);
+        CameraUpdate zoom = CameraUpdateFactory.zoomTo(16);
+
+        map.moveCamera(center);
+
+
+        polylines = new ArrayList<>();
+        if (polylines.size() > 0) {
+            for (Polyline poly : polylines) {
+                poly.remove();
+            }
+        }
+
+        //add route(s) to the map.
+        for (int k = 0; k < route.size(); k++) {
+            int colorIndex = k % COLORS.length;
+
+            Route route1 = route.get(k);
+            PolylineOptions polyOptions = new PolylineOptions();
+            polyOptions.color(getResources().getColor(COLORS[colorIndex]));
+            polyOptions.width(10 + k * 3);
+            polyOptions.addAll(route.get(k).getPoints());
+            Polyline polyline = map.addPolyline(polyOptions);
+            polylines.add(polyline);
+
+            Notification notification = new Notification.Builder(MainActivity.this).setContentTitle("Best Route")
+                    .setContentText("Route " + (k + 1) + ": distance - " + route.get(k).getDistanceValue() + ": duration - " + route.get(k).getDurationValue()).setSmallIcon(R.drawable.common_google_signin_btn_icon_dark_normal).build();
+
+            notification.flags|=Notification.FLAG_AUTO_CANCEL;
+            notificationManager.notify(0,notification);
+            Toast.makeText(getApplicationContext(), "Route " + (k + 1) + ": distance - " + route.get(k).getDistanceValue() +
+                    ": duration - " + route.get(k).getDurationValue(), Toast.LENGTH_SHORT).show();
+        }
+
+        // Start marker
+        MarkerOptions options = new MarkerOptions();
+        options.position(sourceLatLng);
+        options.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_close_light));
+        map.addMarker(options);
+
+        // End marker
+        options = new MarkerOptions();
+        options.position(destination.getLatLng());
+        options.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_cast_off_light));
+        map.addMarker(options);
+        addGeoFence();
+    }
+
+    public void addGeoFence(){
+        LatLng latLng = destination.getLatLng();
+        Geofence geofence = new Geofence.Builder().setRequestId("first")
+                .setCircularRegion(latLng.latitude+5,latLng.longitude+5,10)
+                .setExpirationDuration(10000)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER|Geofence.GEOFENCE_TRANSITION_EXIT).build();
+
+    }
+
+    @Override
+    public void onRoutingCancelled() {
+
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onResult(@NonNull Status status) {
+        
     }
 }
